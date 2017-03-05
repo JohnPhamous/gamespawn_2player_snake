ef9137ba8e5ccf9165701e96c81df6c7adf0c1e8
John Pham <johnphammail@gmail.com>
2017-03-04 18:30:27 -0800

    move

---
+++ lost snakes/Assets/Snake2.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake2 : MonoBehaviour {
+
+	Vector2 dir = Vector2.right;
+	List<Transform> tail = new List<Transform>();
+	bool ate = false;
+	public GameObject tailPrefab;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Move", 0.1f, 0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) {
+			if (dir == Vector2.left) {
+				return;
+			}
+			dir = Vector2.right;
+		}
+		else if (Input.GetKey(KeyCode.S)) {
+			if (dir == Vector2.up) {
+				return;
+			}
+			dir = -Vector2.up;
+		}
+		else if (Input.GetKey(KeyCode.A)) {
+			if (dir == Vector2.right) {
+				return;
+			}
+			dir = -Vector2.right;
+		}
+		else if (Input.GetKey(KeyCode.W)) {
+			if (dir == -Vector2.up) {
+				return;
+			}
+			dir = Vector2.up;
+		}
+	}
+	/*
+	The head changes position. Creates a new tail object
+	where the head was. Deletes the last tail object.
+	 */
+	void Move() {
+		// saves current position 
+		Vector2 v = transform.position;
+		// GetComponent<Rigidbody2D>().MovePosition(dir);
+		// Move head in new direction based on input
+		transform.Translate(dir);
+
+		if (ate) {
+			GameObject g = (GameObject)Instantiate(tailPrefab, v, Quaternion.identity);
+			tail.Insert(0, g.transform);
+			ate = false;
+		}
+		else if (tail.Count > 0) {
+			// Moves last tail object to where head was
+			tail.Last().position = v;
+			// adds a tail object to the front of the list
+			tail.Insert(0, tail.Last());
+			tail.RemoveAt(tail.Count - 1);
+		}
+	}
+	void onTriggerEnter2D(Collider2D coll) {
+		if (coll.name.StartsWith("FoodPrefab")) {
+			ate = true;
+			Destroy(coll.gameObject);
+		}
+	}
+}
---
+++ lost snakes/Assets/SpawnFood.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour {
+	// Food Prefab
+	public GameObject foodPrefab;
+
+	// Border locations
+	public Transform border_top;
+	public Transform border_bottom;
+	public Transform border_left;
+	public Transform border_right;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", 3, 4);
+	}
+	void Spawn() {
+		int x = (int)Random.Range(border_left.position.x + 2, border_right.position.x - 2);
+		int y = (int)Random.Range(border_bottom.position.y - 2, border_top.position.y + 2);
+
+		Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity);
+	}
+}
